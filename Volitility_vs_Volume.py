30fc9b8bca7506f9e5f6166b96e86bbc30420cf1
megz-mugz <69176595+megz-mugz@users.noreply.github.com>
2021-01-23 11:00:10 -0500

    Add files via upload

---
+++ Volitility_vs_Volume.py
@@ -0,0 +1,23 @@
+import datetime as dt
+import pandas_datareader.data as web
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+plt.style.use('dark_background')
+
+
+start = dt.datetime(2000, 1, 1)
+now = dt.datetime.now()
+ticker = input('enter a stock/etf ticker: ')
+df = web.DataReader(ticker, 'yahoo', start, now)
+
+df['Volitility'] = abs((df['Adj Close'] - df['Open']) / df['Open'])
+
+x = df['Volitility']
+y = df['Volume']
+
+sns.jointplot(x='Volitility', y='Volume', data=df, color='red', kind='scatter')
+
+
+plt.show()
