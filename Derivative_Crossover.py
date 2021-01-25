7246fc9d9511419f2beb3136efdebaece17bc3da
megz-mugz <69176595+megz-mugz@users.noreply.github.com>
2021-01-25 10:26:00 -0500

    Add files via upload

---
+++ Derivative_Crossover.py
@@ -0,0 +1,46 @@
+import sympy as sym
+import datetime as dt
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+
+plt.style.use('dark_background')
+
+ticker = 'qqq'
+start = dt.datetime(2018, 5, 1)
+end = dt.datetime.now()
+
+df = web.DataReader(ticker, 'yahoo', start, end)
+
+df['Volatility'] = (df['Adj Close'] - df['Open']) / df['Open']
+
+df['90sma'] = df['Adj Close'].rolling(window=20).mean()
+df['20sma'] = df['Adj Close'].rolling(window=20).mean()
+df['10sma'] = df['Adj Close'].rolling(window=10).mean()
+
+class DataVisualization:
+
+    def mpl(self):
+        fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True)
+        dv = ax[0]
+        price = ax[1]
+
+        df['90sma_derivative'] = sym.diff(df['90sma'])
+        df['90sma_derivative_20sma'] = df['90sma_derivative'].rolling(window=13).mean()
+        df['90sma_derivative_10sma'] = df['90sma_derivative'].rolling(window=5).mean()
+
+        # dv.plot(df.index, df['90sma_derivative'], color='white', label="d/dx 90 sma")
+        dv.plot(df.index, df['90sma_derivative_20sma'], color='red', label='20 sma')
+        dv.plot(df.index, df['90sma_derivative_10sma'], color='green', label='10 sma')
+        dv.set_title('Derivative Crossover & {} Chart'.format(ticker.upper()))
+        dv.legend(loc='best')
+
+        price.plot(df.index, df['Adj Close'])
+        price.plot(df.index, df['20sma'])
+        price.plot(df.index, df['10sma'])
+
+
+
+        plt.show()
+
+object = DataVisualization
+object.mpl(DataVisualization)
